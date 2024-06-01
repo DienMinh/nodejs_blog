e89a21add8b20eb99cf16a3da0c14700dcd93ced
Chó <diennm.hn@gmail.com>
2024-06-01 11:51:18 +0700

    Refactor: create routes, controllers

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ const { engine: handlebars } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.urlencoded({
+  extended: true,
+})); // Form send
+app.use(express.json()); //Js send
+
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template engine
 app.engine(
@@ -21,13 +28,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/home", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
